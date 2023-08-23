0311a541c0eeebc37a4e4e73f24ffb1f41be11f7
Marc Brolly <marcrobertjohn@gmail.com>
2023-08-23 11:25:23 +0100

    Fix issue with ngrok not being properly authenticated and style exercises history component

--- client/src/components/Exercises/ExerciseHistory.js
+++ client/src/components/Exercises/ExerciseHistory.js
@@ -4,48 +4,66 @@ import useExerciseSetsContext from "../../hooks/useExerciseSetsContext";
 
 export default function ExerciseHistory({ exercise }) {
   const { state } = useExerciseSetsContext();
-  const [filteredData, setFilteredData] = useState(state);
-  const [filter, setFilter] = useState("");
 
-  useEffect(() => {
-    const filteredData = filter
-      ? state?.map((ex) => ex.exerciseName === exercise.name)
-      : state;
-    setFilteredData(filteredData);
-  }, [filter, state]);
+  const exercises = state.filter((ex) => ex.exerciseName === exercise.name);
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={filteredData}
+        data={exercises}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => {
           return (
-            <View>
-              <Text>{item.exerciseName}</Text>
+            <View style={styles.listContainer}>
+              <Text style={styles.date}>Mon, 24 August 2023</Text>
+              <View style={styles.hr} />
+              <View style={styles.headerContainer}>
+                <Text style={styles.setText}>Set</Text>
+                <Text style={styles.setText}>kg</Text>
+                <Text style={styles.setText}>Reps</Text>
+              </View>
+              <FlatList
+                data={item.sets}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item }) => {
+                  return (
+                    <View style={styles.headerContainer}>
+                      <Text style={styles.setText}>{item.set}</Text>
+                      <Text style={styles.setText}>{item.kg}</Text>
+                      <Text style={styles.setText}>{item.reps}</Text>
+                    </View>
+                  );
+                }}
+              />
             </View>
           );
         }}
       />
-      <Text style={styles.date}>Mon, 24 August 2023</Text>
-      <View style={styles.hr} />
-      <View style={styles.headerContainer}>
-        <Text style={styles.setText}>Set</Text>
-        <Text style={styles.setText}>kg</Text>
-        <Text style={styles.setText}>Reps</Text>
-      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  listContainer: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: "5%",
+    marginBottom: 10,
+  },
   hr: {
     borderBottomColor: "black",
     borderBottomWidth: StyleSheet.hairlineWidth,
+    marginVertical: 10,
   },
-  date: {},
+  date: {
+    textAlign: "center",
+  },
   setHeading: {},
   setText: {},
-  headerContainer: {},
+  headerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
 });
