e573c90c38a0dad08f19185fd0c29c0b60a89bf0
Marc Brolly <marcrobertjohn@gmail.com>
2023-08-28 00:32:01 +0100

    Add default text to create routine component

--- client/src/components/Routines/RoutineList.js
+++ client/src/components/Routines/RoutineList.js
@@ -1,22 +1,70 @@
-import { StyleSheet, FlatList, Text, View } from "react-native";
+import {
+  StyleSheet,
+  FlatList,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import useRoutineContext from "../../hooks/useRoutineContext";
 import RoutineShow from "./RoutineShow";
+import { useNavigation } from "@react-navigation/native";
 
 export default function RoutineList({ limit }) {
   const { allRoutines } = useRoutineContext();
+  const navigation = useNavigation();
 
   return (
     <View>
-      <FlatList
-        data={allRoutines.slice(0, limit)}
-        key={(item) => item._id}
-        renderItem={({ item }) => {
-          return <RoutineShow routine={item} />;
-        }}
-      />
+      {allRoutines.length >= 1 ? (
+        <>
+          <FlatList
+            data={allRoutines.slice(0, limit)}
+            key={(item) => item._id}
+            renderItem={({ item }) => {
+              return <RoutineShow routine={item} />;
+            }}
+          />
+        </>
+      ) : (
+        <View style={styles.defaultContainer}>
+          <Text style={styles.deafultText}>
+            You have currently not created any routines.
+          </Text>
+          <Text style={styles.deafultText}>
+            Please press the button below or use the plus icon in the header to
+            create a new routine.
+          </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("CreateRoutine")}>
+            <Text style={styles.buttonText}>Create Routine</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  defaultContainer: {},
+  deafultText: {
+    fontSize: 15,
+    marginVertical: 5,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#D5A8F8",
+    borderWidth: 1,
+    borderRadius: "5%",
+    paddingVertical: 8,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "500",
+    textAlign: "center",
+    justifyContent: "flex-end",
+  },
+});
